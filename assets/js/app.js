de88922bdfb8d68cc440486ef65e7a3165b1839e
Edimilson Borges <104403198+EdimilsonBorges@users.noreply.github.com>
2024-01-29 16:21:53 -0300

    feat: salvando tema no localStore

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,9 @@ const btnCriptografar = document.getElementById("btn__criptografar");
 const btnDescriptografar = document.getElementById("btn__descriptografar");
 const sectionResultado = document.getElementById("resultado");
 const themeSwitch = document.getElementById("switch");
-let themeDarck = false;
+let themeDarck = localStorage.getItem("theme");
+
 sectionResultado.appendChild(criarMensagemNaoEncontrado());
-
-
 
 themeSwitch.addEventListener("change",(event)=>{
     if(themeDarck){
@@ -15,42 +14,6 @@ themeSwitch.addEventListener("change",(event)=>{
         darkTheme();
     }
 });
-
-function darkTheme() {
-    themeDarck = true;
-    const img = document.querySelector("#resultado__mensagem__empty > img");
-    if(img){
-        img.setAttribute("src", "./assets/img/empty-dark.svg");
-    }
-
-    const rootStyle = document.documentElement.style;
-    rootStyle.setProperty('--primary-color', '#20200F');
-    rootStyle.setProperty('--text-primary-color',' #FFFFFF');
-    rootStyle.setProperty('--secondary-color', '#939F93');
-    rootStyle.setProperty('--tertiary-color', '#4F4C4C');
-    rootStyle.setProperty('--button-secondary-color', '#C2CFC2');
-    rootStyle.setProperty('--button-primary-hover-color', '#9F9393');
-    rootStyle.setProperty('--text-resultado-color', '#fff');
-    rootStyle.setProperty('--text-secondary-color', '#fff');
-}
-
-function liteTheme(){
-    themeDarck = false;
-    const img = document.querySelector("#resultado__mensagem__empty > img");
-    if(img){
-        img.setAttribute("src", "./assets/img/empty-lite.svg");
-    }
-    const rootStyle = document.documentElement.style;
-    rootStyle.setProperty('--primary-color', '#0A3871');
-    rootStyle.setProperty('--text-primary-color',' #FFFFFF');
-    rootStyle.setProperty('--secondary-color', '#F3F5FC');
-    rootStyle.setProperty('--tertiary-color', '#FFFFFF');
-    rootStyle.setProperty('--button-secondary-color', '#D8DFE8');
-    rootStyle.setProperty('--button-primary-hover-color', '#2a578b');
-    rootStyle.setProperty('--text-resultado-color', '#495057');
-    rootStyle.setProperty('--text-secondary-color', '#000000');
-}
-
 
 btnCriptografar.addEventListener("click", () => {
     const texto = principalTextarea.value;
@@ -71,42 +34,6 @@ btnDescriptografar.addEventListener("click", () => {
         mostrarMensagemDescriptografado();
     }
 });
-
-function criptografarTexto(texto) {
-
-    const textoCriptografado = texto
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    return textoCriptografado;
-}
-
-function descriptografar(texto) {
-
-    const textoDescriptografado = texto
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    return textoDescriptografado;
-}
-
-function copiarParaAreaDeTranferencia() {
-    const texto = document.getElementById("resultado__textarea").value;
-    const mensagem = document.getElementById("mensagem_area_de_tranferencia");
-    navigator.clipboard.writeText(texto).then(function () {
-        mensagem.innerText = "Copiado para a área de transferência.";
-        mensagem.style.opacity = 1;
-        setTimeout(() => { mensagem.style.opacity = 0 }, 1000);
-    }).catch(function (erro) {
-        mensagem.innerText = "Erro ao copiar para a área de transferência.";
-        mensagem.style.opacity = 1;
-        setTimeout(() => { mensagem.style.opacity = 0 }, 1000);
-    });
-}
 
 function mostrarMensagemVazio() {
     const resultadoText = document.getElementById("resultado__textarea");
@@ -182,8 +109,12 @@ function criarMensagemNaoEncontrado() {
 
     const img = document.createElement("img");
     if(themeDarck){
+        themeSwitch.checked = true;
+        darkTheme();
         img.setAttribute("src", "./assets/img/empty-dark.svg");
     }else{
+        themeSwitch.checked = false;
+        liteTheme();
         img.setAttribute("src", "./assets/img/empty-lite.svg");
     }
     img.setAttribute("alt", "Imagem de não encontrado");
@@ -199,4 +130,77 @@ function criarMensagemNaoEncontrado() {
     resultadoEmpty.appendChild(p);
 
     return resultadoEmpty;
+}
+
+function criptografarTexto(texto) {
+
+    const textoCriptografado = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    return textoCriptografado;
+}
+
+function descriptografar(texto) {
+
+    const textoDescriptografado = texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    return textoDescriptografado;
+}
+
+function copiarParaAreaDeTranferencia() {
+    const texto = document.getElementById("resultado__textarea").value;
+    const mensagem = document.getElementById("mensagem_area_de_tranferencia");
+    navigator.clipboard.writeText(texto).then(function () {
+        mensagem.innerText = "Copiado para a área de transferência.";
+        mensagem.style.opacity = 1;
+        setTimeout(() => { mensagem.style.opacity = 0 }, 1000);
+    }).catch(function (erro) {
+        mensagem.innerText = "Erro ao copiar para a área de transferência.";
+        mensagem.style.opacity = 1;
+        setTimeout(() => { mensagem.style.opacity = 0 }, 1000);
+    });
+}
+
+function darkTheme() {
+    localStorage.setItem("theme", "themeDarck");
+    themeDarck = true;
+    const img = document.querySelector("#resultado__mensagem__empty > img");
+    if(img){
+        img.setAttribute("src", "./assets/img/empty-dark.svg");
+    }
+
+    const rootStyle = document.documentElement.style;
+    rootStyle.setProperty('--primary-color', '#20200F');
+    rootStyle.setProperty('--text-primary-color',' #FFFFFF');
+    rootStyle.setProperty('--secondary-color', '#939F93');
+    rootStyle.setProperty('--tertiary-color', '#4F4C4C');
+    rootStyle.setProperty('--button-secondary-color', '#C2CFC2');
+    rootStyle.setProperty('--button-primary-hover-color', '#9F9393');
+    rootStyle.setProperty('--text-resultado-color', '#fff');
+    rootStyle.setProperty('--text-secondary-color', '#fff');
+}
+
+function liteTheme(){
+    localStorage.removeItem("theme","themeDarck");
+    themeDarck = false;
+    const img = document.querySelector("#resultado__mensagem__empty > img");
+    if(img){
+        img.setAttribute("src", "./assets/img/empty-lite.svg");
+    }
+    const rootStyle = document.documentElement.style;
+    rootStyle.setProperty('--primary-color', '#0A3871');
+    rootStyle.setProperty('--text-primary-color',' #FFFFFF');
+    rootStyle.setProperty('--secondary-color', '#F3F5FC');
+    rootStyle.setProperty('--tertiary-color', '#FFFFFF');
+    rootStyle.setProperty('--button-secondary-color', '#D8DFE8');
+    rootStyle.setProperty('--button-primary-hover-color', '#2a578b');
+    rootStyle.setProperty('--text-resultado-color', '#495057');
+    rootStyle.setProperty('--text-secondary-color', '#000000');
 }
